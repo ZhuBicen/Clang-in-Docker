8b41f392dfa35b9f1ac570bb16f2682d4433cea1
bicen.zhu <bicen.zhu@nokia-sbell.com>
2020-03-06 10:38:59 +0800

    va_opt

---
+++ Tests/va_opt.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// #define LOG(msg, ...) printf(msg __VA_OPT__(,) __VA_ARGS__)
+#define LOG(msg, ...) printf(msg, __VA_ARGS__)
+
+int main() {
+    LOG("hello world\n");   // => printf("hello world")
+    LOG("hello world\n", );  // => printf("hello world")
+    LOG("hello %d\n", 0);   // => printf("hello %d", n)
+
+    printf("Hello world",);
+
+    return 0;
+}
